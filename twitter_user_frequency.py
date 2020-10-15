5863cb814509edee878dbf2cb4f90b2045b1bbc9
aariassanta <aariassanta@gmail.com>
2020-10-15 13:29:23 +0200

    2ª Version

--- twitter_user_frequency.py
+++ twitter_user_frequency.py
@@ -3,6 +3,10 @@
 import sys 
 from collections import Counter 
 import json 
+from stop_words import get_stop_words
+
+additional_stop_words =['-','&amp;']
+stop_words = get_stop_words('english') + get_stop_words('spanish') + additional_stop_words
  
 def get_screen_name(tweet): 
   user = tweet.get('user', {})  
@@ -13,6 +17,11 @@ def get_mentioned_name(tweet):
   entities = tweet.get('entities', {}) 
   mentions = entities.get('user_mentions', []) 
   return [tag['screen_name'].lower() for tag in mentions] 
+
+def get_hashtags(tweet): 
+  entities = tweet.get('entities', {}) 
+  hashtags = entities.get('hashtags', []) 
+  return [tag['text'].lower() for tag in hashtags] 
 
 if __name__ == '__main__': 
     fname = sys.argv[1] 
@@ -85,3 +94,36 @@ if __name__ == '__main__':
         print("{}: {}".format(tag, count)) 
 
     print('\n')
+
+    with open(fname, 'r') as f: 
+        hashtags = Counter() 
+        for line in f: 
+            tweet = json.loads(line) 
+            hashtags_in_tweet = get_hashtags(tweet) 
+            hashtags.update(hashtags_in_tweet) 
+
+    print('\n' + '------------ 20 most common hashtags' + '\n')
+    for tag, count in hashtags.most_common(20): 
+        print("{}: {}".format(tag, count)) 
+
+    print('\n')
+
+    # Get most repeated words from list of tweets
+
+    with open(fname, 'r') as f: 
+        name = Counter() 
+        #name = {}
+        for line in f: 
+            tweet = json.loads(line) 
+            #print(name_in_tweet)
+            [ name.update([word]) for word in tweet['text'][4:].lower().split() if not word in stop_words ]
+            #if tweet['text'][:2] == 'RT':
+            #    name.update([tweet['text'][4:].split(':')[0]])
+
+    print('\n' + '------------ 20 most repeated words' + '\n')
+    for tag, count in name.most_common(20): 
+        print("{}: {}".format(tag, count)) 
+
+    print('\n')
+
+    #filtered_sentence = [w for w in word_tokens if not w in stop_words] 
